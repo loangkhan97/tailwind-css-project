e5562aa3705df61e86845cbae3d7102e30c47f48
loangkhan97 <solangisohailkhan903@gmail.com>
2023-09-21 00:42:17 -0700

    all files updated and my portfolio is now completed

--- src/components/UI/Contact.jsx
+++ src/components/UI/Contact.jsx
@@ -2,7 +2,61 @@
 import React from 'react'
 
 const Contact = () => {
-  return 
+  return <section id='contact' className='pb-16'>
+    <div className="container">
+      <h2 className='text-headingColor font-[700] text-[2.5rem] mb-8'>
+        Get in touch
+      </h2>
+      <div className="md:flex justify-between items-center">
+        <div className="w-full md:w-1/2 h-[300px] sm:h-[450px]">
+        <iframe
+        title='google-maps'
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3531.7768950105537!2d68.
+        8160769761305!3d27.72417397617296!2m3!1f0!2f0!3f0!3m2!
+        1i1024!2i768!4f13.1!3m3!1m2!1s0x3935d5d08c5e775f%3A0x605f309fed533539!
+        2sFasTech%20Systems%20(Private)%20Limited!5e0!3m2!1sen!2s!4v1695274815068!5m2!1sen!2s" 
+        className='border-0 w-full h-full'
+        allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
+        </div>
+        <div className="w-full mt-8 md:mt-0 md:w-1/2 sm:h-[450px] lg:flex items-center bg-indigo-100 px-4 lg:px-8">
+          <form className="w-full">
+            <div className="mb-5">
+              <input 
+              type="text"
+              placeholder='Enter your name'
+              className="w-full p-3 focus:outline-none rounded-[5px]"
+              />
+              </div>
+              <div className="mb-5">
+              <input 
+              type="email"
+              placeholder='Enter your email'
+              className="w-full p-3 focus:outline-none rounded-[5px]"
+              />
+              </div>
+              <div className="mb-5">
+              <input 
+              type="text"
+              placeholder='Subject'
+              className="w-full p-3 focus:outline-none rounded-[5px]"
+              />
+            </div>
+            <div className="mb-5">
+             <textarea 
+             type="text" 
+             rows={3}
+             placeholder='Write your message'
+             className='w-full p-3 focus:outline-none rounded-[5px]'
+             />
+            </div>
+
+            <button className='w-full p-3 focus:outline-none rounded-[5px] bg-smallTextColor text-white hover:bg-headingColor text-center ease-linear duration-150'>Send Message</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </section>
 }
 
 export default Contact
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,11 +3,110 @@
 import React from 'react'
 
 const Footer = () => {
+
+  const year = new Date().getFullYear()
+
   return (
-    <div>
-      footer
+  <footer className='bg-[#12141e] pt-12'>
+    {/*============Footer Top============== */}
+    <div className="container">
+      <div className="sm:flex items-center justify-between md:gap-8">
+        <div className="w-full sm:w-1/2">
+          <h2 className='text-[26px] leading-10 text-white font-[600] mb-5 md:text-[2rem]'>
+            Do you want to make beautiful products?
+          </h2>
+          <a href="#contact">
+            <button className='bg-primaryColor text-white font-[500] flex items-center gap-2 hover:bg-smallTextColor ease-in duration-300 py-2 px-4 rounded-[8px] '><i class="ri-mail-line"></i> Hire me</button>
+          </a>
+        </div>
+
+        <div className="w-full sm:w-1/2">
+          <p className="text-gray-300 leading-7 mt-4 sm:mt-0">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Laborum dolor est omnis commodi et at alias cum accusantium 
+            maiores eum. Quos repudiandae vero iusto, ipsum deserunt optio eos.
+            Impedit, exercitationem!
+          </p>
+          <div className="flex items-center gap-4 flex-wrap md:gap-8 mt-10">
+            <span className='text-gray-300 font-[600] text-[15px]'>
+              Follow Me:
+            </span>
+            <span 
+            className="w-[35px] h-[35px] bg-[#2b2d33] p-1 rounded-[50px] cursor-pointer text-center">
+              <a href="#github" className="text-gray-300 font-[500] text-[18px">
+                <i class="ri-github-line"></i>
+                </a>
+            </span>
+            <span 
+            className="w-[35px] h-[35px] bg-[#2b2d33] p-1 rounded-[50px] cursor-pointer text-center">
+              <a href="#youtube" className="text-gray-300 font-[500] text-[18px">
+              <i class="ri-youtube-line"></i>
+                </a>
+            </span>
+            <span 
+            className="w-[35px] h-[35px] bg-[#2b2d33] p-1 rounded-[50px] cursor-pointer text-center">
+              <a href="#linkedIn" className="text-gray-300 font-[500] text-[18px">
+              <i class="ri-linkedin-fill"></i>
+                </a>
+            </span>
+            <span 
+            className="w-[35px] h-[35px] bg-[#2b2d33] p-1 rounded-[50px] cursor-pointer text-center">
+              <a href="#facebook" className="text-gray-300 font-[500] text-[18px">
+              <i class="ri-facebook-fill"></i>
+                </a>
+            </span>
+            <span 
+            className="w-[35px] h-[35px] bg-[#2b2d33] p-1 rounded-[50px] cursor-pointer text-center">
+              <a href="#instagram" className="text-gray-300 font-[500] text-[18px">
+              <i class="ri-instagram-line"></i>
+                </a>
+            </span>
+          </div>
+        </div>
+      </div>
+      <div>
+                <ul className='flex items-center justify-center gap-10 mt-10'>
+                    <li><a className='text-gray-400 font-[600]' href="#about">About</a></li>
+                    <li><a className='text-gray-400 font-[600]' href="#services">Services</a></li>
+                    <li><a className='text-gray-400 font-[600]' href="#portfolio">Portfolio</a></li>
+                    <li><a className='text-gray-400 font-[600]' href="#contact">Contact</a></li>
+                </ul>
+            </div>
     </div>
-  )
-}
+    {/*============Footer Top end============== */}
+
+    {/*============Footer bottom ============== */}
+    <div className="bg-[31b1e29] py-5 mt-14">
+      <div className="container">
+        <div className="flex items-center justify-center sm:justify-between">
+          <div className="hidden sm:block">
+            <div className="flex items-center gap-[10px]">
+              <span className="w-[35px] h-[35px] rounded-full bg-[#2b2d33] text-white font-[500] text-[10px] flex items-center justify-center">
+                L
+              </span>
+
+              <div className="leading-[20px]">
+                <h2 className='text-gray-200 font-[500] text-[18px]'>
+                Loang
+                </h2>
+                <p className="text-gray-400 text-[14px] font-[500]">
+                  Personal
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <p className='text-gray-400 text-[14px]'>Copyright {year} developed by Loang - All right reserved.</p>
+          </div>
+
+        </div>
+      </div>
+    </div>
+    {/*============Footer bottom end============== */}
+
+  </footer>
+  );
+};
 
 export default Footer
